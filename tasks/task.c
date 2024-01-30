451d5730ab4a62160d2fa3170ed0872eb544ea8a
tarik <123542696+tarikkirgin@users.noreply.github.com>
2024-01-30 15:15:02 +0000

    add code

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main() {
+int string_is_number(char* string)
+{
+    for (size_t i = 0; i < strlen(string); i++)
+    {
+        if(!isdigit(string[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 3 || !string_is_number(argv[1]) || !string_is_number(argv[2]))
+    {
+        printf("Usage: task <integer> <integer>\n");
+        return 1;
+    }
+    
     int num1, num2;
-
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
     // Calculate and print the sum
     int sum = num1 + num2;
